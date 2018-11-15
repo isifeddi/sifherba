8701ec3a4d5f6941a4bfa1ad31f4675d9cc0120c
Othman Herba <oherba@e1r12p4.1337.ma>
2018-11-15 21:24:13 +0100

    ana oherba

--- fillit copy/ft_place_tetri.c
+++ fillit copy/ft_place_tetri.c
@@ -6,7 +6,7 @@
 /*   By: oherba <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 18:11:18 by oherba            #+#    #+#             */
-/*   Updated: 2018/11/15 16:55:34 by oherba           ###   ########.fr       */
+/*   Updated: 2018/11/15 21:23:35 by oherba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,6 @@ char **ft_point(char **str, int x)
 	return (str);
 }
 
-
 void	ft_place_one_tetri(t_coor *lstpos, int y, int x, char  **board, char alpha)
 {
 	int i;
@@ -69,49 +68,78 @@ void	ft_place_one_tetri(t_coor *lstpos, int y, int x, char  **board, char alpha)
 	board[lstpos->tab[3].y + y][lstpos->tab[3].x + x] = alpha;
 }
 
+int		check_place(char **board, t_coor *lstpos, int y, int x)
+{
+	while (board[lstpos->tab[0].y + y][lstpos->tab[0].x + x] != '.' || board[lstpos->tab[1].y + y][lstpos->tab[1].x + x] != '.' ||board[lstpos->tab[2].y + y][lstpos->tab[2].x + x] != '.' || board[lstpos->tab[3].y + y][lstpos->tab[3].x + x] != '.')
+	{
+		x++;
+		if (x == 2)
+		{
+			x = 0;
+			y++;
+		}
+	}
+	if ((board[lstpos->tab[0].y + y][lstpos->tab[0].x + x] == '.') && (board[lstpos->tab[1].y + y][lstpos->tab[1].x + x] == '.') && (board[lstpos->tab[2].y + y][lstpos->tab[2].x + x] == '.') && (board[lstpos->tab[3].y + y][lstpos->tab[3].x + x] == '.'))
+		return (1);
+	else 
+		return (0);
+}
+
+char 	**ft_alloc(char **board)
+{
+	int i;
+
+	i = 0;
+   	board = malloc(sizeof(char *) * 20);
+   	while (i < 20)
+	{
+		      board[i]  = malloc(sizeof(char) * 20);
+		        i++;
+	}
+	return (board);
+}
+
+void	ft_print_res(char **board, int i)
+{
+	int j;
+
+	j = 0;
+	while (j < i)
+	    {
+		         ft_putstr("\n");
+		         ft_putstr_zero(board[j]);
+		         j++;
+		 }
+}
+
 void	ft_place(t_coor *lstpos)
 {
 	int x;
 	int y;
 	char alpha;
 	int i;
-	int j;
 	char    **board;
 
 	x = 0;
 	y = 0;
-	i = 0;
-	j = 0;
+	i = 3;
 	alpha = 'A';
-	board = malloc(sizeof(char *) * 20);
-	while (i < 20)
-	{
-		board[i]  = malloc(sizeof(char) * 20);
-		i++;
-	}
+    board =	ft_alloc(board);
 	ft_initialise_zero(board);
-	ft_point(board, 3);
+	ft_point(board, i);
 	while(lstpos)
 	{
-		while (board[lstpos->tab[0].y + y][lstpos->tab[0].x + x] != '.' || board[lstpos->tab[1].y + y][lstpos->tab[1].x + x] != '.' ||
-				board[lstpos->tab[2].y + y][lstpos->tab[2].x + x] != '.' || board[lstpos->tab[3].y + y][lstpos->tab[3].x + x] != '.')
+		if (check_place(board,lstpos, y, x) == 1)
 		{
-			x++;
-			if (x == 2)
-			{
-				x = 0;
-				y++;
-			}
+			ft_place_one_tetri(lstpos,y,x,board, alpha);
+			alpha++;
+			lstpos = lstpos->next;
 		}
-		ft_place_one_tetri(lstpos,y,x,board, alpha);
-		lstpos = lstpos->next;
-		alpha++;
+		else
+		{
+			i++;
+			ft_point(board,i);
+		}
 	}
-
-	while (j < 2)
-	{
-		ft_putstr_zero("\n");
-		ft_putstr(board[j]);
-		j++;
-	}
+	ft_print_res(board, i);
 }
